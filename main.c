eb11bb94acb886ff7d44d6f42faa6604c2b93722
sparkblame <1062409423@qq.com>
2019-11-08 17:29:57 +0800

    as

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N 8
+int main ()
+{
+    int a[N]={6,3,7,9,2,1,5,4};
+    for (int i = 1; i < N; i++)
+    {
+        int j,key=a[i];
+        for (j = i-1; j >= 0; j--)
+        {
+            if (a[j]>key)
+            {   
+                a[j+1]=a[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        a[j+1]=key;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        printf ("%d ",a[i]);
+    }
+    printf("\n");
+    
+    printf("hello world!");
+}
